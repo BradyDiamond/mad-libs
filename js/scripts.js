311bf69c3d4e64df23af0b9ca2e993257cfe6a18
Brady Diamond <brdydmnd@gmail.com>
2022-03-28 16:21:02 -0700

    add array to logic
    Co-authored-by: Will Pratt <willpratt78@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,24 @@ $(document).ready(function() {
       const exclamationInput = $("input#exclamation").val();
       const verbInput = $("input#verb").val();
       const nounInput = $("input#noun").val();
-  
-      $(".person1").text(person1Input);
-      $(".person2").text(person2Input);
-      $(".animal").text(animalInput);
-      $(".exclamation").text(exclamationInput);
-      $(".verb").text(verbInput);
-      $(".noun").text(nounInput);
-  
+      
+      let array1 = []
+      array1.push(person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput)
+      array1.forEach(function(element) {
+      $(".person1").text(array1[0]);
+      $(".person2").text(array1[1]);
+      $(".animal").text(array1[2]);
+      $(".exclamation").text(array1[3]);
+      $(".verb").text(array1[4]);
+      $(".noun").text(array1[5]);
+    });
+
       $("#story").show();
     });
     $("#formTwo").submit(function(event) {
       event.preventDefault();
       const myNameInput = $("input#myName").val();
-
+ 
       $(".myName").text(myNameInput);
 
       $("#letter").show();
